4337f78ef7c3850512d4b9e0696f30014f584a29
Trend20 <enockomondi305@gmail.com>
2022-09-23 12:42:47 +0300

    adding and rendering images

--- src/app/products/products-list/products-list.component.ts
+++ src/app/products/products-list/products-list.component.ts
@@ -8,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class ProductsListComponent implements OnInit {
 
   pageTitle: string = 'Products List';
+  imageWidth: number = 50;
+  imageMargin: number = 2;
   products: any[] = [
     {
       "productId": 1,
